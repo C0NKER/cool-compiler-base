54bd8709a5285a3d18b765c54df65348cb0e2cb3
d.garcia <d.garciabfd@gmail.com>
2019-10-14 09:44:08 -0400

    Update tests

--- tests/semantic_test.py
+++ tests/semantic_test.py
@@ -7,6 +7,6 @@ tests = [(tests_dir + file) for file in os.listdir(tests_dir) if file.endswith('
 
 @pytest.mark.semantic
 @pytest.mark.error
-@pytest.mark.parametrize("cool_file", tests)
+@pytest.mark.parametrize("cool_file", [])
 def test_semantic_errors(compiler_path, cool_file):
     compare_errors(compiler_path, cool_file, cool_file[:-3] + '_error.txt')
